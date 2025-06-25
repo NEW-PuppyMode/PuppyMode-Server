aa60aebccec9722d2accfbf2ac7271f37c359b04
alschlee <1daysixmeal@gmail.com>
2025-06-25 13:42:24 +0900

    [CHORE] 프로젝트 생성

---
+++ src/main/java/com/umc/puppymode2/Puppymode2Application.java
@@ -0,0 +1,13 @@
+package com.umc.puppymode2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Puppymode2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Puppymode2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/umc/puppymode2/Puppymode2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.puppymode2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Puppymode2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
