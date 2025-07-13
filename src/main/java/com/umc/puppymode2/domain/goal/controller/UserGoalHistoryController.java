cb31ace0b4238f78d0db9bcf34f2c2d7996588d6
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2025-07-13 22:57:16 +0900

    [FEAT] 목표 관련 API 응답 형식 보완 #9

--- src/main/java/com/umc/puppymode2/domain/goal/controller/UserGoalHistoryController.java
+++ src/main/java/com/umc/puppymode2/domain/goal/controller/UserGoalHistoryController.java
@@ -6,11 +6,14 @@ import com.umc.puppymode2.domain.goal.dto.GoalPostResponseDTO;
 import com.umc.puppymode2.domain.goal.service.UserGoalHistoryCommandService;
 import com.umc.puppymode2.domain.goal.service.UserGoalHistoryQueryService;
 import com.umc.puppymode2.domain.user.service.KakaoAuthService;
+import com.umc.puppymode2.global.apiPayload.ApiResponse;
+import com.umc.puppymode2.global.auth.context.SecurityUserContext;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,43 +24,44 @@ public class UserGoalHistoryController {
 
     private final UserGoalHistoryCommandService commandService;
     private final UserGoalHistoryQueryService queryService;
+    private final SecurityUserContext securityUserContext;
 
     private Long getCurrentUserId() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println("authentication.getName() = " + authentication.getName());
-        if (authentication == null || !authentication.isAuthenticated()) {
-            throw new RuntimeException("인증 정보가 없습니다.");
-        }
-
-        return Long.parseLong(authentication.getName());
+        return securityUserContext.getCurrentUserId();
     }
 
     // 1. 목표 등록 (신규/유지)
     @PostMapping
     @Operation(summary = "목표 등록 API", description = "목표 등록 API 입니다. 새로운 목표는 true, 기존 목표 유지는 false를 넣어주시면 됩니다.")
-    public ResponseEntity<GoalPostResponseDTO> postGoal(
+    public ResponseEntity<ApiResponse<GoalPostResponseDTO>>postGoal(
             @RequestBody @Valid GoalPostRequestDTO requestDto) {
 
         Long userId = getCurrentUserId();
         GoalPostResponseDTO response = commandService.postGoal(userId, requestDto);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(
+                ApiResponse.onSuccess(response, "GOAL200", "목표 등록 성공")
+        );
     }
 
     // 2. 최근 목표 조회
     @GetMapping
     @Operation(summary = "최근 목표 조회 API", description = "최근 목표 조회 API 입니다.")
-    public ResponseEntity<GoalInfoResponseDTO> getLatestGoal() {
+    public ResponseEntity<ApiResponse<GoalInfoResponseDTO>> getLatestGoal() {
         Long userId = getCurrentUserId();
         GoalInfoResponseDTO response = queryService.getLatestGoal(userId);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(
+                ApiResponse.onSuccess(response, "GOAL200", "최근 목표 조회 성공")
+        );
     }
 
     // 3. 목표 설정 후 30일 경과 여부
     @GetMapping("/check-30days")
     @Operation(summary = "목표 설정 후 30일 경과 여부 API", description = "목표 설정 후 30일 경과 여부 API 입니다.")
-    public ResponseEntity<Boolean> check30Days() {
+    public ResponseEntity<ApiResponse<Boolean>> check30Days() {
         Long userId = getCurrentUserId();
         boolean isPassed = queryService.isMoreThan30DayPassed(userId);
-        return ResponseEntity.ok(isPassed);
+        return ResponseEntity.ok(
+                ApiResponse.onSuccess(isPassed, "GOAL200", "목표 설정 후 30일 경과 여부 조회 성공")
+        );
     }
 }
