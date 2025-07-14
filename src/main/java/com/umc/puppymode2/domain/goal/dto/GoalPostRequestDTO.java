f809f7b1297f5a4d676baa19e247f197516ddc68
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2025-07-14 21:17:48 +0900

    [REFACTOR] 코드 리뷰 반영 예외처리 및 API 수정 #9

--- src/main/java/com/umc/puppymode2/domain/goal/dto/GoalPostRequestDTO.java
+++ src/main/java/com/umc/puppymode2/domain/goal/dto/GoalPostRequestDTO.java
@@ -1,7 +1,5 @@
 package com.umc.puppymode2.domain.goal.dto;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
