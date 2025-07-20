ed83ad83a86e3c71fbbf1ab4d6d667b5a993545f
YYJ <yoon20220823@dukcung.ac.kr>
2025-07-20 14:52:05 +0900

    [FEAT] 메인 화면 조회 API êµ

--- src/main/java/com/umc/puppymode2/domain/puppy/repository/PuppyRepository.java
+++ src/main/java/com/umc/puppymode2/domain/puppy/repository/PuppyRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-// 관련된 엔티티/enum/repository 가 미리 생성되지 않아 임시로 구현한 파일입니다.
-// 이후 다른 분들 코드도 머지되면, 해당 코드 속 파일로 import 변경하도록 하겠습니다!
 public interface PuppyRepository extends JpaRepository<Puppy, Long> {
     Optional<Puppy> findByUser_UserId(Long userId);
 }
---
+++ src/main/java/com/umc/puppymode2/domain/puppy/service/MainService.java
@@ -0,0 +1,7 @@
+package com.umc.puppymode2.domain.puppy.service;
+
+import com.umc.puppymode2.domain.puppy.dto.MainResponseDto;
+
+public interface MainService {
+    MainResponseDto getMainPageInfo();
+}
