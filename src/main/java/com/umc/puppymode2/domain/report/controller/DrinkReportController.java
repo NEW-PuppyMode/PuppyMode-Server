10d00ead9f3d7ee4073abde2ff20838a62f751f3
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2025-10-25 14:51:02 +0900

    [REFACTOR] 리포트 컨트롤러 Validated 어노테이션 추가 #68

--- src/main/java/com/umc/puppymode2/domain/report/controller/DrinkReportController.java
+++ src/main/java/com/umc/puppymode2/domain/report/controller/DrinkReportController.java
@@ -9,6 +9,7 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.umc.puppymode2.global.auth.context.UserContext;
 import java.time.YearMonth;
 
+@Validated
 @RestController
 @RequestMapping("/report")
 @RequiredArgsConstructor
