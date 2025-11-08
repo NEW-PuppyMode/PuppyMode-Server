6740005678d6ea3c98ac30731b2cc25f130696fa
leesj <jinycap7@gmail.com>
2025-11-08 15:19:42 +0900

    [FEAT] PuppyType enum 필드 수정 #80

--- src/main/java/com/umc/puppymode2/domain/onboarding/dto/OnboardingTestResDTO.java
+++ src/main/java/com/umc/puppymode2/domain/onboarding/dto/OnboardingTestResDTO.java
@@ -3,8 +3,9 @@ package com.umc.puppymode2.domain.onboarding.dto;
 public record OnboardingTestResDTO(
 
         String type, // 예: "분위기 리더형"
-        String puppyBreed, // 예: "비숑 프리제"
-        String description, // 타입에 대한 설명
+        String puppyBreedKo, // 예: "비숑 프리제"
+        String puppyBreedEn, // 예: "Bichon Frisé"
+        String description, // 타입 설명, 예: "술자리를 이끄는 사람들"
         String imageUrl // 강아지 이미지 url
 ) {
 }
--- src/main/java/com/umc/puppymode2/domain/puppy/entity/PuppyType.java
+++ src/main/java/com/umc/puppymode2/domain/puppy/entity/PuppyType.java
@@ -5,19 +5,21 @@ import lombok.Getter;
 @Getter
 public enum PuppyType {
 
-    BICHON("분위기 리더형", "비숑", "술자리를 이끄는 활발하고 감성적인 리더!", "https://example.image.url.com"),
-    SHIBA("은밀한 취객형", "시바견", "혼술을 사랑하는 섬세한 감성가!", "https://example.image.url.com"),
-    CORGI("컨트롤러형", "웰시코기", "술을 도구처럼 다루는 전략가!", "https://example.image.url.com"),
-    POODLE("관찰자형", "푸들", "술자리에서도 분석과 기록을 놓치지 않는 관찰자!", "https://example.image.url.com");
+    BICHON("분위기 리더형", "비숑 프리제", "Bichon Frisé", "술자리를 이끄는 사람들", "https://puppy-mode-s3-bucket.s3.ap-northeast-2.amazonaws.com/bichon_default_level1.svg"),
+    SHIBA("은밀한 취객형", "시바견", "Shiba Inu", "혼술 장인", "https://puppy-mode-s3-bucket.s3.ap-northeast-2.amazonaws.com/shiba_default_level1.svg"),
+    CORGI("컨트롤러형", "웰시코기", "Welsh Corgi", "술은 도구, 취함은 선택", "https://puppy-mode-s3-bucket.s3.ap-northeast-2.amazonaws.com/corgi_default_level1.svg"),
+    POODLE("관찰자형", "푸들", "Poodle", "분석하고 기록하는 술자리 학자들", "https://puppy-mode-s3-bucket.s3.ap-northeast-2.amazonaws.com/poodle_default_level1.svg");
 
     private final String type; // 성향 이름
-    private final String breed; // 견종
-    private final String description; // 소개 문구
+    private final String breedKo; // 견종 (한글명)
+    private final String breedEn; // 견종 (영문명)
+    private final String description; // 성향 설명
     private final String imageUrl; // 강아지 대표 이미지
 
-    PuppyType(String type, String breed, String description, String imageUrl) {
+    PuppyType(String type, String breedKo, String breedEn, String description, String imageUrl) {
         this.type = type;
-        this.breed = breed;
+        this.breedKo = breedKo;
+        this.breedEn = breedEn;
         this.description = description;
         this.imageUrl = imageUrl;
     }
