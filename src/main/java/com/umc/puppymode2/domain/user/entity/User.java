49e0c4c7d0c7e60cab254a6ab01d9753aa5fa300
한지수 <90364562+eldeoddt@users.noreply.github.com>
2025-12-11 05:02:24 +0900

    [FIX] provider varchar로 수정 #97

--- src/main/java/com/umc/puppymode2/domain/user/entity/User.java
+++ src/main/java/com/umc/puppymode2/domain/user/entity/User.java
@@ -29,6 +29,7 @@ public class User extends BaseEntity {
     private String email;
 
     @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(20)")
     private Provider provider;
 
     private Boolean receiveNotifications;
